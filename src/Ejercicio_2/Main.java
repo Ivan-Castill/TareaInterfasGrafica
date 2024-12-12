9675a59dedd95510d90e376cd0a4afb74e08410e
Ivan-Castill <ivan2003castillo12345@gmail.com>
2024-12-11 23:05:45 -0500

    Ejercicio 2

--- src/Ejercicio_2/Main.java
+++ src/Ejercicio_2/Main.java
@@ -1,4 +1,17 @@
 package Ejercicio_2;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    public static void  main(String[] args){
+        System.out.println("Ejecutando Programa (°w°)");
+        JFrame frame = new JFrame("Programa de Login");
+        frame.setContentPane(new Ventana().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,300);
+        frame.setPreferredSize(new Dimension(500,300));
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
--- src/Ejercicio_2/Ventana.java
+++ src/Ejercicio_2/Ventana.java
@@ -1,4 +1,36 @@
 package Ejercicio_2;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Ventana {
+    private JLabel labelTitulo;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton buttonlogin;
+    private JLabel labelUsuario;
+    private JLabel labelPassword;
+    public JPanel mainPanel;
+
+    public Ventana() {
+        buttonlogin.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String usuarioCorrecto = "apellido";
+                String passwordCorrecto = "apellido123";
+
+                // Obtiene los datos ingresados por el usuario
+                String usuarioIngresado = textField1.getText();
+                String passwordIngresado = new String(passwordField1.getPassword());
+
+                // Validación de las credenciales
+                if (usuarioIngresado.equals(usuarioCorrecto) && passwordIngresado.equals(passwordCorrecto)) {
+                    JOptionPane.showMessageDialog(null, "BIENVENIDO\n"+usuarioCorrecto+"\n Inicio de sesión exitoso");
+                } else {
+                    JOptionPane.showMessageDialog(null, "Usuario o contraseña incorrectos", "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+    }
 }
