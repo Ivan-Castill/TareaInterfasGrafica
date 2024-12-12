9e2b15cb98da808a9da3f4782ed17a2bde017a3f
Ivan-Castill <ivan2003castillo12345@gmail.com>
2024-12-11 23:05:36 -0500

    Ejercicio 1

--- src/Ejercicio_1/Main.java
+++ src/Ejercicio_1/Main.java
@@ -1,5 +1,17 @@
+package Ejercicio_1;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        JFrame frame = new JFrame(" Calculadora Chida ");
+        frame.setContentPane(new Ventana().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,500);
+        frame.setPreferredSize(new Dimension(400,500));
+        frame.pack();
+        frame.setVisible(true);
     }
 }
--- src/Ejercicio_1/Ventana.java
+++ src/Ejercicio_1/Ventana.java
@@ -1,4 +1,53 @@
 package Ejercicio_1;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Ventana {
+    private JLabel labelTitulo;
+    private JButton buttonSuma;
+    private JButton buttonResta;
+    private JButton buttonMultiplicacion;
+    private JButton buttonDivicion;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JLabel labelTotal;
+    private JLabel labelResult;
+    public JPanel mainPanel;
+
+    public Ventana() {
+        buttonSuma.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Double num1 = Double.parseDouble(textField1.getText());
+                Double num2 = Double.parseDouble(textField2.getText());
+                labelResult.setText(String.valueOf(num1 + num2));
+            }
+        });
+        buttonResta.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Double num1 = Double.parseDouble(textField1.getText());
+                Double num2 = Double.parseDouble(textField2.getText());
+                labelResult.setText(String.valueOf(num1 - num2));
+            }
+        });
+        buttonMultiplicacion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Double num1 = Double.parseDouble(textField1.getText());
+                Double num2 = Double.parseDouble(textField2.getText());
+                labelResult.setText(String.valueOf(num1 * num2));
+            }
+        });
+        buttonDivicion.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Double num1 = Double.parseDouble(textField1.getText());
+                Double num2 = Double.parseDouble(textField2.getText());
+                labelResult.setText(String.valueOf(num1 / num2));
+            }
+        });
+    }
 }
